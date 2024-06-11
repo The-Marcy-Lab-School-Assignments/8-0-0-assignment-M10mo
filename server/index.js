6f8ca8a79693e0c745dedee52357e2ce90b61764
M10mo <mohdkanoongo@gmail.com>
2024-06-11 15:37:56 -0400

    resub changes

--- server/index.js
+++ server/index.js
@@ -1,33 +1,71 @@
+// const express = require("express");
+// const app = express();
+// const PORT = 8080;
+
+// function logger(req, res, next) {
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// }
+
+// function sendHtmlResponse(req, res, next) {
+//   res.send("<h1>Hello, World!</h1>");
+// }
+// // this should be different than the query param one
+// function sendDataResponse(req, res, next) {
+//   res.json({ message: "Hello, World!" });
+// }
+// // you did not make a about.html file so  it will not work you will get an error
+// function sendHtmlFile(req, res, next) {
+//   res.sendFile(__dirname + "/about.html");
+// }
+
+// function sendDataWithQueryParam(req, res, next) {
+//   const name = req.query.name || "World";
+//   res.json({ message: `Hello, ${name}!` });
+// }
+// // you should run middleware before the routes. No need to run them in the argument.
+// app.get("/", logger, sendHtmlResponse);
+// app.get("/about", logger, sendHtmlFile);
+// app.get("/api/data", logger, sendDataResponse);
+// app.get("/api/greeting", logger, sendDataWithQueryParam);
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const path = require("path");
+const PORT = 3000;
 
 function logger(req, res, next) {
   console.log(`${req.method} ${req.url}`);
   next();
 }
 
+app.use(logger);
+
 function sendHtmlResponse(req, res, next) {
   res.send("<h1>Hello, World!</h1>");
 }
-// this should be different than the query param one
+
 function sendDataResponse(req, res, next) {
   res.json({ message: "Hello, World!" });
 }
-// you did not make a about.html file so  it will not work you will get an error
+
 function sendHtmlFile(req, res, next) {
-  res.sendFile(__dirname + "/about.html");
+  res.sendFile(path.join(__dirname, "about.html"));
 }
 
 function sendDataWithQueryParam(req, res, next) {
   const name = req.query.name || "World";
   res.json({ message: `Hello, ${name}!` });
 }
-// you should run middleware before the routes. No need to run them in the argument.
-app.get("/", logger, sendHtmlResponse);
-app.get("/about", logger, sendHtmlFile);
-app.get("/api/data", logger, sendDataResponse);
-app.get("/api/greeting", logger, sendDataWithQueryParam);
+
+app.get("/", sendHtmlResponse);
+app.get("/about", sendHtmlFile);
+app.get("/api/data", sendDataResponse);
+app.get("/api/greeting", sendDataWithQueryParam);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
